c95cc1ae2952ffaecd0ad561bb5e6cd17d1f8b32
NSD <nsd24technology@gmail.com>
2020-07-20 23:41:16 +0900

    display firestore data

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,21 @@
     <button class="button is-primary is-rounded" @click="login">
       ログイン
     </button>
+
+    <table class="table is-narrow">
+      <thead>
+        <tr>
+          <th>todo</th>
+          <th>limit</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="todo in $store.getters.getTodos" :key="todo.todo">
+          <td>{{ todo.todo }}</td>
+          <td>{{ todo.limit }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -14,6 +29,9 @@ export default {
       console.log('login')
       this.$store.dispatch('login')
     },
+  },
+  created() {
+    this.$store.dispatch('fetchTodos')
   },
 }
 </script>
